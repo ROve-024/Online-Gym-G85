d44c350039e1cd636e52ce4d4ac9159733d5b9d3
CUI <newtrevor.peloz@outlook.com>
2021-04-23 10:45:47 +0800

    Add test file, modify class name

---
+++ Test/Java/org/qmbupt/grp85/gui/AdminGuiTest.java
@@ -0,0 +1,10 @@
+package org.qmbupt.grp85.gui;
+import GUI.Admin.ALLLessonAdmin;
+import org.junit.jupiter.api.Test;
+
+public class AdminGuiTest {
+    @Test
+    public void testAllLessonAdmin(){
+        ALLLessonAdmin.run();
+    }
+}
---
+++ Test/Java/org/qmbupt/grp85/gui/OtherGuiTest.java
@@ -0,0 +1,4 @@
+package org.qmbupt.grp85.gui;
+
+public class OtherGuiTest {
+}
